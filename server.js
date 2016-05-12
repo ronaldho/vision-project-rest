f43582c298f3d1f9acf66643419fe34dda768ae4
Ronald Ho <dev@ronaldho.me>
2016-05-11 17:19:02 -0700

    First iteration of server, with get drugs and get drugs by id

---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var DRUGS_COLLECTION = "drugs";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+
+// API Routes Below
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/drugs"
+ *    GET: finds all drugs
+ *
+ */
+app.get("/drugs", function(req, res){
+    db.collection(DRUGS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get drugs.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+
+/*  "/drugs/:id"
+ *    GET: find drug by id
+ *
+ *
+ */
+
+app.get("/drugs/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get drug");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
